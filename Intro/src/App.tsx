263195f12a7bac29f6f921fbe0e85b18922c5537
Shahriar-Samir <shabusiness035@gmail.com>
2024-12-31 23:25:53 +0600

    Intro - add: increment, decrement handlers with dispatch function

--- Intro/src/App.tsx
+++ Intro/src/App.tsx
@@ -1,4 +1,17 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "../redux/features/counter/counterSlice";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch(increment());
+  };
+
+  const decrementHandler = () => {
+    dispatch(decrement());
+  };
+
   return (
     <main className="bg-[#63c0ca] h-[100vh] flex justify-center items-center text-white">
       <div className="shadow-md shadow-[#3e335f52] p-5 rounded-lg flex flex-col items-center w-11/12 max-w-max">
@@ -7,10 +20,16 @@ const App = () => {
         </h1>
         <h2 className="text-3xl font-bold mt-10">0</h2>
         <div className="gap-5 flex justify-between mt-10">
-          <button className="rounded px-4 py-2 shadow-sm transition-all font-semibold shadow-[#29214244]">
+          <button
+            onClick={incrementHandler}
+            className="rounded px-4 py-2 shadow-sm transition-all font-semibold shadow-[#29214244]"
+          >
             Increment
           </button>
-          <button className="rounded px-4 py-2 shadow-sm font-semibold shadow-[#29214244]">
+          <button
+            onClick={decrementHandler}
+            className="rounded px-4 py-2 shadow-sm font-semibold shadow-[#29214244]"
+          >
             Decrement
           </button>
         </div>
