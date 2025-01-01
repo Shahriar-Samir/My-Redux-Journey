7e9d4819cc5c5de52623a9d03e6ab815e873c005
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 09:57:51 +0600

    Practices/Practice2 - add: dispatches and count state on view

--- Practices/practice2/src/App.tsx
+++ Practices/practice2/src/App.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { decrement, increment } from "../redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hook";
 
 const App = () => {
+  const { count } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
+
   return (
     <main>
       <h1>Counter</h1>
-      <h2>0</h2>
-      <button>Increment</button>
-      <button>Increment by 5</button>
-      <button>decrement</button>
+      <h2>{count}</h2>
+      <button onClick={() => dispatch(increment({ count: 1 }))}>
+        Increment
+      </button>
+      <button onClick={() => dispatch(increment({ count: 5 }))}>
+        Increment by 5
+      </button>
+      <button onClick={() => dispatch(decrement({ count: 1 }))}>
+        decrement
+      </button>
     </main>
   );
 };
