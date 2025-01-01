0d2d836d52c6fbc0bbd8e72f267e1ea262c6d5d4
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 23:28:01 +0600

    Practices/Task-Management-Project - update: navbar ui

--- Task-Mangement-project/client/src/components/layout/navbar.tsx
+++ Task-Mangement-project/client/src/components/layout/navbar.tsx
@@ -1,7 +1,12 @@
 const Navbar = () => {
-  return <nav className="w-11/12 max-w-[700px] flex justify-between">
-    
-  </nav>;
+  return (
+    <nav className="w-11/12 max-w-[700px] flex justify-between mx-auto py-6">
+      <section className="flex gap-2 items-center">
+        <img src="/icon.png" className="w-[40px] h-[40px]" />
+        <h1 className="text-xl font-bold">Task-Manager</h1>
+      </section>
+    </nav>
+  );
 };
 
 export default Navbar;
