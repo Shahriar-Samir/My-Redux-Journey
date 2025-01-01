07d77e4468f7629e92cd41d76cbbbd60778829c2
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-01 08:23:26 +0600

    Practices add Practice1 Project

---
+++ Practices/practice1/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <h3>0</h3>
+      <button>Increment</button>
+      <button>Increment by 5</button>
+      <button>Decrement</button>
+    </div>
+  );
+};
+
+export default App;
