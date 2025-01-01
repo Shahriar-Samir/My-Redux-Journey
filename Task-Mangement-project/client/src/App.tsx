997712a5f410e47230528579d978503282edfa88
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-02 00:10:01 +0600

    Practices/Task-Management-Project - remove: theme provider from app route

--- Task-Mangement-project/client/src/App.tsx
+++ Task-Mangement-project/client/src/App.tsx
@@ -1,16 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/layout/navbar";
 import { Button } from "./components/ui/button";
-import { ThemeProvider } from "./components/theme-provider";
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <main>
-        <Navbar />
-        <Outlet />
-      </main>
-    </ThemeProvider>
+    <main>
+      <Navbar />
+      <Outlet />
+    </main>
   );
 };
 
