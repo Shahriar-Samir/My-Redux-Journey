8a741a3e6e5d0c8375d62ff70756ec8d55ea06b6
Shahriar-Samir <shabusiness035@gmail.com>
2025-01-02 07:01:28 +0600

    Practices/Task-Management-Project - add: redux provider

--- Task-Mangement-project/client/src/main.tsx
+++ Task-Mangement-project/client/src/main.tsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes/routes.tsx";
 import { ThemeProvider } from "./providers/theme-provider.tsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <RouterProvider router={routes} />
+      <Provider store={store}>
+        <RouterProvider router={routes} />
+      </Provider>
     </ThemeProvider>
   </StrictMode>
 );
